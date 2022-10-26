43916338056615eff5b63acb9b270a835e98fa51
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2022-10-26 11:50:58 +0000

    Wed, Oct 26, 2022, 11:50 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,13 @@
+//---------------------------------
+//Markus Dwiyanto Tobi Sogen
+//CS50 for teachers
+//Indonesia
+//mario-more.c
+//---------------------------------
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+
 }
