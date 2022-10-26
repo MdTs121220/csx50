5ef8dddbc8bb0cdab5fa8041a434bed96e4dfe72
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2022-10-26 09:39:12 +0000

    Wed, Oct 26, 2022, 9:39 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,15 @@
+//---------------------------------
+//Markus Dwiyanto Tobi Sogen
+//CS50 for teachers
+//Indonesia
+//Hello.c
+//---------------------------------
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello, world\n");
+    string name = get_string("What is your name: "); //deklarasi variable string
+
+    printf("hello, %s\n", name); //output print
 }
