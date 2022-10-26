8ad51e00014c6c51ce1955a60d4698a5541545c7
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2022-10-26 11:51:47 +0000

    Wed, Oct 26, 2022, 11:51 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -9,5 +9,33 @@
 
 int main(void)
 {
+    int height;
 
+    do
+    {
+        height = get_int("Height: ");  //Input height first
+    }
+
+    while
+    ((height < 1) || (height > 8));  // boolean logic use OR
+
+    // for loop
+    for (int row = 0; row < height; row++)
+
+    {
+
+        for (int column = 0; column < height; column++) // piramid create
+        {
+            if (row + column >= height - 1)
+            {
+                printf("#");
+            }
+
+            else
+            {
+                printf(" "); //space
+            }
+        }
+        printf("\n");
+    }
 }
