f29b6cbbddacb29129f0539eb578c63ed48f44cc
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2022-10-27 10:12:40 +0000

    Thu, Oct 27, 2022, 10:12 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,7 +5,7 @@ int main(void)
 {
    // Input card number
     long n = get_long("Number: ");
-    
+
     // Calculate the length
     int i = 0;
     long l = n;
@@ -14,40 +14,43 @@ int main(void)
         l = l / 10;
         i++;
     }
-    // Check whether length is valid
-    if (i != 13 && i != 15 && i != 16)
+
+    if (i != 13 && i != 15 && i != 16) // Check length number if valid
     {
         printf("INVALID\n");
         return 0;
     }
+
     // Calculate sum
-    int sum1 = 0;
-    int sum2 = 0;
+    int jum1 = 0;
+    int jum2 = 0;
     long x = n;
     int total = 0;
     int d1;
     int d2;
-    int mod1;
-    int mod2;
+    int modu1;
+    int modu2;
     do
     {
         // Remove last digit
-        mod1 = x % 10;
+        modu1 = x % 10;
         x = x / 10;
-        sum1 = sum1 + mod1;
+        jum1 = jum1 + modu1;
+
         // Remove second last digit
-        mod2 = x % 10;
+        modu2 = x % 10;
         x = x / 10;
+
         // 2 times second last digit and add digits to sum2
-        mod2 = mod2 * 2;
-        d1 = mod2 % 10;
-        d2 = mod2 / 10;
-        sum2 = sum2 + d1 + d2;
+        modu2 = modu2 * 2;
+        d1 = modu2 % 10;
+        d2 = modu2 / 10;
+        jum2 = jum2 + d1 + d2;
     }
     while (x > 0);
-    total = sum1 + sum2;
-    // Checking Luhn Algorithm
-    if (total % 10 != 0)
+    total = jum1 + jum2;
+
+    if (total % 10 != 0) // Checking
     {
         printf("INVALID\n");
         return 0;
@@ -59,6 +62,7 @@ int main(void)
         start = start / 10;
     }
     while (start > 100);
+
     // Checking starting digits for the card type
     if ((start / 10 == 5) && (0 < start % 10 && start % 10 < 6))
     {
