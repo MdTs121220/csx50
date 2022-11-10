75fa20c3821e8ca14e4078265cf3ea1ff20ffcf4
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2022-11-10 06:46:27 +0000

    Thu, Nov 10, 2022, 6:46 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -51,6 +51,14 @@ int main(void)
     {
         printf("Before Grade 1\n");
     }
+    else if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 
 
 }
