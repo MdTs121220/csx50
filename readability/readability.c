cc3deb10d56176ec397285cb3457489d26b02c78
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2022-11-10 06:34:36 +0000

    Thu, Nov 10, 2022, 6:34 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,29 @@
+//---------------------------------
+//Markus Dwiyanto Tobi Sogen
+//CS50 for teachers
+//Indonesia
+//readability.c
+//---------------------------------
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
 
-int main(void) 
+int main(void)
 {
+    int count_letter = 0;
+    int count_word = 1;
+    int count_sentence = 0;
+
+    string text = get_string("Text: ");
+
+    int text_leng = strlen(text);
+
+    for (int 1 = 0; i < text_leng; i++)
+    {
+        if()
+    }
+
 
 }
