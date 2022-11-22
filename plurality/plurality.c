ea6740bbc179696ea393c9e197b07179896a3c34
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2022-11-22 04:50:27 +0000

    Tue, Nov 22, 2022, 4:50 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,15 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    // Check names of candidate
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
