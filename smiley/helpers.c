da4e5e0a4dfdf5afa2ce812089ea34fdbb352eb5
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2022-12-02 15:20:40 +0000

    Fri, Dec 2, 2022, 3:20 PM +00:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -7,7 +7,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // Make black pixels turn red
+            // change black to red
             if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
             {
                 image[i][j].rgbtRed = 0xff;
