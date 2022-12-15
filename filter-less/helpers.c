dab44f0f27ddf9c541645c4d52b3c7857443b404
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2022-12-15 23:23:54 +0000

    Thu, Dec 15, 2022, 11:23 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,25 +1,155 @@
+//---------------------------------
+//Markus Dwiyanto Tobi Sogen
+//CS50 for teachers
+//Indonesia
+//Filter-less.c
+//---------------------------------
 #include "helpers.h"
+#include "math.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    //iterate through each pixel kolom dan baris
+    int average;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int Red = image[i][j].rgbtRed;
+            int Green = image[i][j].rgbtGreen;
+            int Blue = image[i][j].rgbtBlue;
+            average = round(((float)Red + (float)Green + (float)Blue) / 3);
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
     return;
 }
 
-// Convert image to sepia
+//konvert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    //iterate through the height (kolom)
+    int originalRed, originalGreen, originalBlue;
+    int sepiaRed, sepiaBlue, sepiaGreen;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++) //iterate through the width (baris)
+        {
+            //try assign variable array
+            originalRed = image[i][j].rgbtRed;
+            originalGreen = image[i][j].rgbtGreen;
+            originalBlue = image[i][j].rgbtBlue;
+
+            //calculate for sepia
+            sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            if (sepiaRed > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
+            else
+            {
+                image[i][j].rgbtRed = sepiaRed;
+            }
+            //calculate for sepiaGreen
+            sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            if (sepiaGreen > 255)
+            {
+                image[i][j].rgbtGreen = 255;
+            }
+            else
+            {
+                image[i][j].rgbtGreen = sepiaGreen;
+            }
+
+            //calculate for sepiaBlue
+            sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue); //Calculate for sepiaBlue
+            if (sepiaBlue > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
+            else
+            {
+                image[i][j].rgbtBlue = sepiaBlue;
+            }
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    //iterate through the height and half of width
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (width / 2); j++)
+        {
+            //create temporary variable
+            int temPred = image[i][j].rgbtRed;
+            int temPgreen = image[i][j].rgbtGreen;
+            int temPblue = image[i][j].rgbtBlue;
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+            image[i][width - j - 1].rgbtRed = temPred;
+            image[i][width - j - 1].rgbtGreen = temPgreen;
+            image[i][width - j - 1].rgbtBlue = temPblue;
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    //create a temporary image blur
+    RGBTRIPLE temp[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int total_Red, total_Blue, total_Green;
+            total_Red = total_Blue = total_Green = 0;
+            float counter = 0.00;
+
+            //Get the next pixel
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    int currentX = i + x;
+                    int currentY = j + y;
+                    if (currentX < 0 || currentX > (height - 1) || currentY < 0 || currentY > (width - 1)) //check next pixel
+                    {
+                        continue;
+                    }
+
+                    total_Red += image[currentX][currentY].rgbtRed;
+                    total_Green += image[currentX][currentY].rgbtGreen;
+                    total_Blue += image[currentX][currentY].rgbtBlue;
+                    counter++;
+                }
+
+                //do the average pf pixel
+                temp[i][j].rgbtRed = round(total_Red / counter);
+                temp[i][j].rgbtGreen = round(total_Green / counter);
+                temp[i][j].rgbtBlue = round(total_Blue / counter);
+            }
+        }
+    }
+
+    //copy the blurr image to image ori
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = temp[i][j].rgbtRed;
+            image[i][j].rgbtGreen = temp[i][j].rgbtGreen;
+            image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
+        }
+    }
     return;
 }
---
+++ recover/recover.c
@@ -0,0 +1,64 @@
+//---------------------------------
+//Markus Dwiyanto Tobi Sogen
+//CS50 for teachers
+//Indonesia
+//Recover.c
+//---------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+typedef uint8_t BYTE;
+
+int main(int argc, char *argv[])
+{
+    //try argc to equal two
+    if (argc != 2)
+    {
+        printf("Usage: ./recover image\n");
+        return 1;
+    }
+    else
+    {
+        // try to open card raw
+        char *input_file_name = argv[1];
+        FILE *input_pointer = fopen(input_file_name, "r"); //input file in read mode
+        if (input_pointer == NULL)
+        {
+            printf("Error: cannot open %s\n", input_file_name);
+            return 2;
+        }
+
+        // Initialise variables byte
+        BYTE buffer[512];
+        int count = 0;
+        FILE *img_pointer = NULL;
+        char filename[8];
+        // Repeat until end
+        while (fread(&buffer, 512, 1, input_pointer) == 1)
+        {
+            // try check If start of a new JPEG (Byte start*):
+            if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+            {
+                // If not, close
+                if (!(count == 0))
+                {
+                    fclose(img_pointer);
+                }
+
+                // Initialise file
+                sprintf(filename, "%03i.jpg", count);
+                img_pointer = fopen(filename, "w");
+                count++;
+            }
+
+            // If JPEG has been found, try write to file
+            if (!(count == 0))
+            {
+                fwrite(&buffer, 512, 1, img_pointer);
+            }
+        }
+        fclose(input_pointer);
+        fclose(img_pointer);
+        return 0;
+    }
+}
