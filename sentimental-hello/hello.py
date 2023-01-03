e003b10d369007e2d4031a356f4c3497b1a53b4c
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-03 11:44:31 +0000

    Tue, Jan 3, 2023, 11:44 AM +00:00

---
+++ sentimental-hello/hello.py
@@ -0,0 +1,10 @@
+# ---------------------------------
+# Markus Dwiyanto Tobi Sogen
+# CS50 for teachers
+# Indonesia
+# Hello.py
+# ---------------------------------
+# TODO
+from cs50 import get_string
+answer = get_string("What is your name? ")
+print("hello, " + answer)
