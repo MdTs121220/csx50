16c38d469044eb3d8201450e6a799feb2004e2e4
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-05 15:01:56 +0000

    Thu, Jan 5, 2023, 3:01 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,1 +1,10 @@
 # TODO
+from cs50 import get_float
+# count coins is used
+count = 0
+# input user
+while True:
+    changeowed = get_float("Change owed: ")
+    if changeowed > 0:
+        break
+cent = round(change * 100)
