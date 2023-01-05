dfbb912efaf08d22421684d359a4e4c25e6b6d9e
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-05 11:58:50 +0000

    Thu, Jan 5, 2023, 11:58 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -6,15 +6,18 @@
 # ---------------------------------
 # TODO
 from cs50 import get_int
-while True: #cek if user input number true or false
+# cek if user input number true or false
+while True:
     height = get_int("Height:  ")
     # try check number input from user
     if height > 0 and height < 9:
         break
 # row
 for row in range(height):
-    for space in range(height - row -1, 0, -1): # try make space
+    # try make space
+    for space in range(height - row - 1, 0, -1):
         print(" ", end="")
-    for hash in range(row + 1): # try make hash
-        print("#", end ="")
-    print("\n", end ="")
+    # try make hash
+    for hash in range(row + 1):
+        print("#", end="")
+    print("\n", end="")
