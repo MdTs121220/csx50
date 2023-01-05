6def47159057594aa6cd6ecbb9be6afd8062ca6f
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-05 15:19:17 +0000

    Thu, Jan 5, 2023, 3:19 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,3 +1,9 @@
+# ---------------------------------
+# Markus Dwiyanto Tobi Sogen
+# CS50 for teachers
+# Indonesia
+# cash.py
+# ---------------------------------
 # TODO
 from cs50 import get_float
 # count coins is used
@@ -8,23 +14,25 @@ while True:
     if changeowed > 0:
         break
 cent = round(changeowed * 100)
+# number of quarters
 while cent >= 25:
     cent = cent - 25
     count += 1
 
+# number of dimes
 while cent >= 10:
     cent = cent - 10
     count += 1
 
+# number of nickels
 while cent >= 5:
     cent = cent - 5
     count += 1
 
+# number of pennies
 while cent >= 1:
     cent = cent - 1
     count += 1
 
 # print number count
 print(count)
-
-
