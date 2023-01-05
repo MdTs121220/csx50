fe28b85e7ada13e5b7fb93c26134e887724b37b1
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-05 16:27:34 +0000

    Thu, Jan 5, 2023, 4:27 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -11,4 +11,25 @@ text_lenght = len(text)
 
 # cek number of letters in text
 for i in range(text_lenght):
-    if(text[i].is)
+    if(text[i].isalpha()):
+        count_letter += 1
+
+# try cek number of words in text input
+    if(text[i].isspace()):
+        count_word +=1
+
+# try cek number of sentence
+    if(text[i] == ',' or text[i] == '?' or text[i] == '!'):
+        count_sentence += 1
+
+# Coleman-Liau index Formula
+calcu = (0.0588 * count_letter / count_word * 100) - (0.296 * count_sentence / count_word * 100) - 15.8
+
+index = round(calcu)
+
+if index < 1:
+    print("Before Grade 1")
+elif index > 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {index}")
