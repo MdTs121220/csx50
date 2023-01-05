1710a7037ef3a79d8844ae76198edb71d7b5826c
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-05 16:20:42 +0000

    Thu, Jan 5, 2023, 4:20 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,14 @@
 # TODO
+from cs50 import get_string
+
+count_letter = 0
+count_word = 1
+count_sentence = 0
+
+# Get input from user
+text = get_string("Text: ")
+text_lenght = len(text)
+
+# cek number of letters in text
+for i in range(text_lenght):
+    if(text[i].is)
