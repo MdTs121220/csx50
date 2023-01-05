773786e95c269e2ec967c701a57ecce6afe58ada
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-05 15:13:44 +0000

    Thu, Jan 5, 2023, 3:13 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -12,5 +12,17 @@ while cent >= 25:
     cent = cent - 25
     count += 1
 
+while cent >= 10:
+    cent = cent - 10
+    count += 1
+
+while cent >= 5:
+    cent = cent - 5
+    count += 1
+
+while cent >= 1:
+    cent = cent - 1
+    count += 1
+
 
 
