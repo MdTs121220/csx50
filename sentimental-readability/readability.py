4bc5a0e04af6cfe4ca10159fa26223b353677d78
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-05 16:55:37 +0000

    Thu, Jan 5, 2023, 4:55 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -13,13 +13,11 @@ textl = len(text)
 for i in range(textl):
     if text[i].isalpha():
         count_letter += 1
-
 # try cek number of words in text input
-    if text[i].isspace():
+    elif text[i].isspace():
         count_word += 1
-
 # try cek number of sentence
-    if text[i] == ',' or text[i] == '?' or text[i] == '!' :
+    elif text[i] == ',' or text[i] == '?' or text[i] == '!' :
         count_sentence += 1
 
 # Coleman-Liau index Formula
