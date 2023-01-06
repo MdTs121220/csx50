f85b7d47a7f9cb8dfb727354fdc14f13b2a6c2fc
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-06 14:24:31 +0000

    Fri, Jan 6, 2023, 2:24 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -29,10 +29,10 @@ def main():
         STR_match[STR[i]] = longest_match(sequence,STR[i])
 
     # TODO: Check database for matching profiles
-    for i in range(len(dna_database)):
+    for person in dna_database:
         matches = 0
-        for j in range(len(STR)):
-            if int(STR_match[STR[j]]) == int(dna_database[i][STR[j]]):
+        for sub_str in STR:
+            if STR_match[sub_str] == int(person[sub_str]):
                 matches += 1
                 if matches == len(STR):
                     print(dna_database[i]['name'])
