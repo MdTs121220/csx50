5a0a9918aec7c3e0c577943d3365793447e92b32
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-07 19:55:45 +0000

    Sat, Jan 7, 2023, 7:55 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,24 @@
+# ---------------------------------
+# Markus Dwiyanto Tobi Sogen
+# CS50 for teachers
+# Indonesia
+# mario-more.py
+# ---------------------------------
 # TODO
+from cs50 import get_int
+# cek if user input number true or false
+while True:
+    height = get_int("Height:  ")
+    # try check number input from user
+    if height > 0 and height < 9:
+        break
+for i in range(height):
+    # Print left side
+    for j in range(i + 1):
+        print("#", end="")
+    # Print space
+    print("  ", end="")
+    # Print right side
+    for j in range(i + 1):
+        print("#", end="")
+    print()
