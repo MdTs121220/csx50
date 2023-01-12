f8efe2cba7aa7dafa435c3ca99fe82a97b8d5e49
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-12 16:18:22 +0000

    Thu, Jan 12, 2023, 4:18 PM +00:00

---
+++ songs/5.sql
@@ -0,0 +1,8 @@
+---------------------------------
+-- Markus Dwiyanto Tobi Sogen
+-- CS50 for teachers
+-- Indonesia
+-- query that returns the average energy of all the songs
+----------------------------------
+
+SELECT avg(energy) FROM songs;
