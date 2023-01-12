18d0d49d7c7bf8f70fd83b1f0d4f98e0676fe976
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-12 16:20:04 +0000

    Thu, Jan 12, 2023, 4:20 PM +00:00

---
+++ songs/6.sql
@@ -0,0 +1,9 @@
+---------------------------------
+-- Markus Dwiyanto Tobi Sogen
+-- CS50 for teachers
+-- Indonesia
+-- query that lists the names of songs that are by Post Malone
+----------------------------------
+
+SELECT name FROM songs
+WHERE artist_id = (SELECT id FROM artists WHERE name == "Post Malone");
---
+++ songs/7.sql
@@ -0,0 +1,8 @@
+---------------------------------
+-- Markus Dwiyanto Tobi Sogen
+-- CS50 for teachers
+-- Indonesia
+-- query that returns the average energy of songs that are by Drake
+----------------------------------
+
+query that returns the average energy of songs that are by Drake
