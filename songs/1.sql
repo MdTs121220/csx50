8e800a84e3a08660070a3231d86aa7c07a36a592
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-12 16:14:39 +0000

    Thu, Jan 12, 2023, 4:14 PM +00:00

--- songs/1.sql
+++ songs/1.sql
@@ -1,3 +1,8 @@
--- query should output a table with a single column for the name of each song
+---------------------------------
+-- Markus Dwiyanto Tobi Sogen
+-- CS50 for teachers
+-- Indonesia
+-- query to list the names of all songs in the database
+----------------------------------
 
 SELECT name FROM songs;
---
+++ songs/2.sql
@@ -0,0 +1,9 @@
+---------------------------------
+-- Markus Dwiyanto Tobi Sogen
+-- CS50 for teachers
+-- Indonesia
+-- query to list the names of all songs in increasing order of tempo
+----------------------------------
+
+SELECT name FROM songs
+ORDER BY tempo;
