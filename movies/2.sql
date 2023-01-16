62bede89194636caacf842fd1b1c3774e6358fb2
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-16 14:40:01 +0000

    Mon, Jan 16, 2023, 2:40 PM +00:00

---
+++ movies/2.sql
@@ -0,0 +1,8 @@
+---------------------------------
+-- Markus Dwiyanto Tobi Sogen
+-- CS50 for teachers
+-- Indonesia
+-- query to determine the birth year of Emma Stone
+----------------------------------
+
+select birth from people where name="Emma Stone";
