9155e4c3bd2b16268ddee9731bcaa3a9a6f6c8a7
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-16 14:59:21 +0000

    Mon, Jan 16, 2023, 2:59 PM +00:00

---
+++ movies/6.sql
@@ -0,0 +1,6 @@
+---------------------------------
+-- Markus Dwiyanto Tobi Sogen
+-- CS50 for teachers
+-- Indonesia
+-- query to list the titles and release years of all Harry Potter movies, in chronological order
+----------------------------------
