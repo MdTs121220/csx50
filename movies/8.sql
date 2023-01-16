e60716fdb3642f1f93b733e5445aa14bd94b8180
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-16 15:20:33 +0000

    Mon, Jan 16, 2023, 3:20 PM +00:00

---
+++ movies/8.sql
@@ -0,0 +1,7 @@
+---------------------------------
+-- Markus Dwiyanto Tobi Sogen
+-- CS50 for teachers
+-- Indonesia
+-- query to list the names of all people who starred in Toy Story
+----------------------------------
+
