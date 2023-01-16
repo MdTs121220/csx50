d2fd3059941e4f913784f87c660286c653c00696
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-16 14:35:45 +0000

    Mon, Jan 16, 2023, 2:35 PM +00:00

--- movies/1.sql
+++ movies/1.sql
@@ -1,4 +1,9 @@
+---------------------------------
+-- Markus Dwiyanto Tobi Sogen
+-- CS50 for teachers
+-- Indonesia
+-- query to list the titles of all movies released in 2008
+----------------------------------
 
----  query to list the titles of all movies released in 2008
 
 select title from movies where year=2008;
