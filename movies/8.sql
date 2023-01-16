964e16cc67ac0d5682a09ac1c0243e7bf9eb9218
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-16 15:22:43 +0000

    Mon, Jan 16, 2023, 3:22 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -5,3 +5,7 @@
 -- query to list the names of all people who starred in Toy Story
 ----------------------------------
 
+select name from people
+join stars on stars.person_id=people.id
+join movies on stars.movie_id=movies.id
+where movies.title="Toy Story";
