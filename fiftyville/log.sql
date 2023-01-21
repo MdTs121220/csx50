071711eefd52e1c33761416a276e029617fd1a8b
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-01-21 21:08:46 +0000

    Sat, Jan 21, 2023, 9:08 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,68 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- step 1 -- filter from table crime
+SELECT description FROM crime_scene_reports
+WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
+
+-- step 2 -- find transkrip about the incidents
+SELECT name, transcript FROM interviews
+WHERE year = 2021 AND month = 7 AND day = 28;
+
+-- step 3 -- found two people have the same name, filtering that name again
+SELECT name FROM people
+WHERE name = 'Eugene';
+
+--step 4 -- try to filter the transcript about bakery from witness to give interviews day of incidents
+SELECT name, transcript FROM interviews
+WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%' ORDER BY name;
+
+--step 5 -- try to check their interview -- start from eugene clues
+SELECT account_number, amount FROM atm_transactions
+WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
+
+--step 6 -- find corresponding account numbers from step 5
+SELECT name, atm_transactions.amount FROM people
+JOIN bank_accounts ON people.id = bank_accounts.person_id
+JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+WHERE atm_transactions.year = 2021 AND atm_transactions.month = 7
+AND atm_transactions.day = 28 AND atm_transactions.atm_location = 'Leggett Street'
+AND atm_transactions.transaction_type = 'withdraw';
+
+-- step 7 -- try to check raymond clues -> Finding the flights on July 29 from Fiftyville airport
+SELECT abbreviation, full_name, city FROM airports
+WHERE city = 'Fiftyville';
+SELECT flights.id, full_name, city, flights.hour, flights.minute FROM airports
+JOIN flights ON airports.id = flights.destination_airport_id
+WHERE flights.origin_airport_id =
+(SELECT id FROM airports WHERE city = 'Fiftyville')
+AND flights.year = 2021
+AND flights.month = 7
+AND flights.day = 29
+ORDER BY flights.hour, flights.minute;
+
+-- step 8 -> Found First flight comes out to be at 8:20 to LaGuardia Airport in New York City (Flight id- 36). maybe found the place where the thief (check the passengers)
+SELECT passengers.flight_id, name, passengers.passport_number, passengers.seat FROM people
+JOIN passengers ON people.passport_number = passengers.passport_number
+JOIN flights ON passengers.flight_id = flights.id
+WHERE flights.year = 2021 AND flights.month = 7 AND flights.day = 29
+AND flights.hour = 8 AND flights.minute = 20
+ORDER BY passengers.passport_number;
+
+--- step 9 -> try to check the phone call
+SELECT name, phone_calls.duration FROM people
+JOIN phone_calls ON people.phone_number = phone_calls.caller
+WHERE phone_calls.year = 2021 AND phone_calls.month = 7 AND phone_calls.day = 28
+AND phone_calls.duration <= 60 ORDER BY phone_calls.duration;
+
+--- step 10 -> check names of the call and name receive
+SELECT name, phone_calls.duration FROM people
+JOIN phone_calls ON people.phone_number = phone_calls.receiver
+WHERE phone_calls.year = 2021 AND phone_calls.month = 7 AND phone_calls.day = 28
+AND phone_calls.duration <= 60 ORDER BY phone_calls.duration;
+
+--- step 11 -> check clues from ruth
+SELECT name, bakery_security_logs.hour, bakery_security_logs.minute FROM people
+JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
+WHERE bakery_security_logs.year = 2021 AND bakery_security_logs.month = 7 AND bakery_security_logs.day = 28
+AND bakery_security_logs.activity = 'exit' AND bakery_security_logs.hour = 10
+AND bakery_security_logs.minute >= 15 AND bakery_security_logs.minute <= 25 ORDER BY bakery_security_logs.minute;
