d035d329887c0c02033290681cd3f9a06d77dd3e
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-02-01 09:45:16 +0000

    Wed, Feb 1, 2023, 6:45 PM +09:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,6 +27,13 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        # Access form data user
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+
+        # Insert data into database
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
         return redirect("/")
 
@@ -34,6 +41,7 @@ def index():
 
         # TODO: Display the entries in the database on index.html
 
+        # Query for all birthdays
+        birthdays = db.execute("SELECT * FROM birthdays")
+
         return render_template("index.html")
-
-
