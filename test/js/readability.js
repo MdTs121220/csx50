f00e77d2b2fbed1895004f96dcfb5df06ffb07b7
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-02-16 14:46:35 +0000

    Thu, Feb 16, 2023, 11:46 PM +09:00

---
+++ test/js/readability.js
@@ -0,0 +1,37 @@
+const readlineSync = require('readline-sync');
+const text = readlineSync.question('Text: ');
+
+let countLetter = 0;
+let countWord = 1;
+let countSentence = 0;
+
+const textLeng = text.length;
+
+for (let i = 0; i < textLeng; i++) {
+  if (text[i].match(/[a-z]/i)) {
+    countLetter++;
+  }
+}
+
+for (let i = 0; i < textLeng; i++) {
+  if (text[i] === ' ') {
+    countWord++;
+  }
+}
+
+for (let i = 0; i < textLeng; i++) {
+  if (text[i] === '.' || text[i] === '?' || text[i] === '!') {
+    countSentence++;
+  }
+}
+
+const calcu = (0.0588 * (countLetter / countWord * 100)) - (0.296 * (countSentence / countWord * 100)) - 15.8;
+const index = Math.round(calcu);
+
+if (index < 1) {
+  console.log('Before Grade 1');
+} else if (index > 16) {
+  console.log('Grade 16+');
+} else {
+  console.log(`Grade ${index}`);
+}
