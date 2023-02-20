b3bf803ccc46e8e711000a8df7def57457e5abaf
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-02-20 12:25:36 +0000

    Mon, Feb 20, 2023, 9:25 PM +09:00

--- test/jack/push.c
+++ test/jack/push.c
@@ -1,4 +1,13 @@
 bool push(int n)
 {
-    // TODO
+    if(s.size < CAPACITY) // check if stack is not full
+    {
+        s.numbers[s.size] = n; // add element to the top of the stack
+        s.size++; // increment the size of the stack
+        return true; // push successful
+        }
+        else
+        {
+            return false; // push failed, stack is full
+            }
 }
