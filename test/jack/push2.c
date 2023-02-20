486947fd0e61b5353d48b5e87da8c2166cb4b527
dwiyanto25 <115984275+dwiyanto25@users.noreply.github.com>
2023-02-20 12:31:45 +0000

    Mon, Feb 20, 2023, 9:31 PM +09:00

--- test/jack/push2.c
+++ test/jack/push2.c
@@ -1,4 +1,27 @@
 bool push(int n)
 {
-    // TODO
+    if(t.numbers == NULL) // check if stack is empty
+    {
+        t.numbers = (int*) malloc(sizeof(int)); // allocate memory for one element
+        if(t.numbers == NULL) // check if memory allocation failed
+        {
+            return false; // push failed, memory allocation failed
+        }
+    }
+    else if(t.size >= SIZE_MAX / sizeof(int) || t.size >= INT_MAX) // check if stack is full
+    {
+        return false; // push failed, stack is full
+    }
+    else
+    {
+        int temp = (int) realloc(t.numbers, (t.size + 1) * sizeof(int)); // allocate more memory for the stack
+        if(temp == NULL) // check if memory allocation failed
+        {
+            return false; // push failed, memory allocation failed
+            }
+            else
+            {
+                t.numbers = temp; // update the stack pointer to the new memory location
+                }
+                }
 }
